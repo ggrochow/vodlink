54625839968b56b757d66a2792ff749363b27a9b
Graham Grochowski <ggrochow@gmail.com>
2019-09-28 14:44:27 -0700

    Sort champion table by name

--- webapp/components/matchup_search/champion_table.js
+++ webapp/components/matchup_search/champion_table.js
@@ -3,8 +3,22 @@ import lolData from "../../../lol_data";
 
 let championIds = Object.keys(lolData.championById);
 
+function sortByName(a, b) {
+    let nameA = lolData.championById[a].name.toLowerCase();
+    let nameB = lolData.championById[b].name.toLowerCase();
+
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+
+    return 0;
+}
+
 const ChampionTable = (props) => {
-    let validChamps = props.validChamps || championIds;
+    let validChamps = (props.validChamps || championIds).sort(sortByName);
 
     return (
 
